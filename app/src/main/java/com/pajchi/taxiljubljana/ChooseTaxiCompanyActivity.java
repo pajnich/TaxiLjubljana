3a0b92c15ebd15fd88593b766012c4b77e6996d2
Matic P <matic.pajnic@gmail.com>
2018-09-20 07:59:56 +0200

    started choose taxi company activity

---
+++ app/src/main/java/com/pajchi/taxiljubljana/ChooseTaxiCompanyActivity.java
@@ -0,0 +1,13 @@
+package com.pajchi.taxiljubljana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseTaxiCompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_taxi_company);
+    }
+}
